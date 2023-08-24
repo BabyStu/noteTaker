4db1e86944ceff87e77fdec7e7e49e46fe3af998
BabyStu <srw33de@gmail.com>
2023-08-24 13:12:01 -0500

    updates

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -42,8 +42,8 @@ const saveNote = (note) =>
     body: JSON.stringify(note),
   });
 
-const deleteNote = (id) =>
-  fetch(`/api/notes/${id}`, {
+const deleteNote = (notes_id) =>
+  fetch(`/api/notes/${notes_id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
@@ -72,6 +72,7 @@ const handleNoteSave = () => {
     text: noteText.value,
   };
   saveNote(newNote).then(() => {
+    console.log('test')
     getAndRenderNotes();
     renderActiveNote();
   });
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,35 +1,82 @@
 const notes = require('express').Router();
-
-const {
-    readFromFile,
-    readAndAppend,
-    writeToFile,
-} = require('../')
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs')
 
 notes.get('/', (req, res) => {
-    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+    res.json(JSON.parse(fs.readFileSync('./db/notes.json', 'utf8')))
 });
 
-notes.get('/')
+// notes.delete('/:note_id', (req, res) => {
+//     console.log('inside delete')
+//     const noteId = req.params.note_id;
+//     console.log(noteId)
+
+//     fs.readFile(./db/notes.json, 'utf8', (readErr, data) => {
+//         console.log(noteId)
+
+//         if (readErr) {
+//             console.error('ERROR', readErr);
+//             return;
+//         }
+
+//         const json = JSON.parse(data);
+
+//         const updatedJson = json.filter(note => note.note_id !== noteId);
+
+//         console.log(noteId)
+
+//         fs.writeFile(./db/notes.json, JSON.stringify(updatedJson), (writeErr) => {
+//             if (writeErr) {
+//                 console.log('ERROR', writeErr);
+//                 return;
+//             }
+
+//             res.json('DELETED NOTE');
+//         });
+//     });
+// });
 
 notes.post('/', (req, res) => {
-    console.log(req.body);
-  
-    const { username, topic, tip } = req.body;
-  
-    if (req.body) {
-      const newTip = {
-        username,
-        tip,
-        topic,
-        tip_id: uuidv4(),
-      };
-  
-      readAndAppend(newTip, './db/notes.json');
-      res.json(`Tip added successfully`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+
+        const noteString = JSON.stringify(newNote)
+
+        fs.readFile('./db/notes.json', 'utf8', (readErr, noteString) => {
+            if (readErr) {
+                console.error('ERROR:', readErr);
+                res.status(500).json({ error: 'An error occurred while reading the file' });
+                return;
+            }
+        
+            let noteParse = JSON.parse(noteString) || [];
+            noteParse.push(newNote);
+        
+            fs.writeFile('./db/notes.json', JSON.stringify(noteParse), (writeErr) => {
+                if (writeErr) {
+                    console.error('ERROR:', writeErr);
+                    res.status(500).json({ error: 'An error occurred while writing the file' });
+                    return;
+                }
+        
+                console.log('Note added!');
+                res.json('Note added!');
+            });
+        });
+        
+        
     } else {
-      res.error('Error in adding tip');
+        console.error('ERROR!');
     }
-  });
+});
+
+
 
 module.exports = notes;
